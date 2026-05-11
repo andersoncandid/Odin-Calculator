33d3c6c110118bdce62f0157fc3bdbf842cf4e18
andersoncandid <190989879+andersoncandid@users.noreply.github.com>
2026-05-11 04:16:44 -0300

    Second Version of script
    Logic is not working yet.

--- script.js
+++ script.js
@@ -1,68 +1,110 @@
-function operate(num1, num2, operator) {
+function operate(previousValue, currentValue, operator) {
+    let result = 0;
+    const num1 = parseInt(previousValue);
+    const num2 = parseInt(currentValue);
+
     switch (operator) {
         case 'add':
-            return num1 + num2;
+            result = num1 + num2;
+            break;
         case 'sub':
-            return num1 - num2;
+            result = num1 - num2;
+            break;
         case 'mult':
-            return num1 * num2;
+            result = num1 * num2;
+            break;
         case 'divi':
-            return num1 / num2;
+            result = num1 / num2;
+            break;
     }
+
+    previousValue = result + '';
+    display.textContent = result + '';
+    currentValue = '';
 }
 
-let currentValue;
-let previousValue;
-let result;
+let numInput = '';
+let previousValue = '0';
+let currentValue = '0';
 let operator = '';
-let currentInput = '';
+let previousOperator = '';
 
 const display = document.querySelector('.display');
-const numpad = document.querySelector('.numpad');
-const operators = document.querySelector('.operators');
-const calcFunctions = document.querySelector('.functions');
+const keys = document.querySelector('.keys');
 
-numpad.addEventListener('click', function (e) {
-    if (e.target.tagName != 'BUTTON') {
+keys.addEventListener('click', function (e) {
+    const target = e.target;
+
+    if (target.tagName != 'BUTTON') {
         return;
     }
 
-    const targetContent = e.target.textContent;
-    const displayContent = display.textContent;
+    // Click on number keys
+    if (target.id === 'num') {
+        const targetContent = e.target.textContent;
+        const displayContent = display.textContent;
 
-    if (targetContent === '+/-') {
-        if (displayContent === '0') {
+        if (targetContent === '+/-') {
+            // Ignore negative zero
+            if (displayContent === '0') {
+                return;
+            }
+            const num = parseInt(displayContent) * -1;
+            numInput = num + '';
+        } else {
+            numInput += targetContent;
+        }
+
+        // Limit length to 18 (max display size)
+        if (numInput.length >= 18) {
+            alert('Maximum number size reached!');
             return;
         }
-        const num = parseInt(displayContent) * -1;
-        currentInput = num + '';
-    } else {
-        currentInput += targetContent;
+
+        display.textContent = numInput;
     }
 
-    // Limit length to 18 (max display size)
-    if (currentInput.length >= 18) {
-        alert('Maximum number size reached!');
-        return;
+    if (target.parentElement.className === 'operators') {
+        // Operator input without any input value
+        if (numInput === '' && currentValue === '' && previousValue === '') {
+            return;
+        }
+
+        if (e.target.id != 'equal') {
+            operator = e.target.id;
+
+            // Normal input
+            if (numInput != '') {
+                currentValue = numInput;
+                previousOperator = operator;
+                numInput = '';
+                return;
+            }
+
+            // Perform the calculation
+            if (numInput != '') {
+                previousValue = currentValue;
+                currentValue = numInput;
+                operate(previousValue, currentValue, previousOperator);
+                previousOperator = operator;
+                numInput = '';
+                return;
+            }
+        } else {
+            if (previousOperator === '') {
+                return;
+            }
+            // Equal entry continuously
+            if (numInput === '') {
+                operate(previousValue, previousValue, previousOperator);
+                return;
+            }
+            operate(previousValue, currentValue, previousOperator);
+            numInput = '';
+        }
     }
 
-    display.textContent = currentInput;
-});
-
-operators.addEventListener('click', function (e) {
-    operator = e.target.id;
-
-    // Capture the first number and reset the display value
-    currentValue = parseInt(currentInput);
-    currentInput = '';
-});
-
-calcFunctions.addEventListener('click', function (e) {
-    const targetContent = e.target.textContent;
-
-    switch (targetContent) {
-        case '=':
-            result = operate(previousValue, currentValue, operator);
-            display.textContent = result + '';
+    // Click on 'clear' keys
+    if (target.id === 'all_clear') {
     }
 });
